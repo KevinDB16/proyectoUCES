3f86c2f906dc0ef744a727f7c4a846c21c71865c
Kevin <kevin.delbello@bue.edu.ar>
2025-06-25 20:16:09 -0300

    Cambios 25-06

--- app/src/main/java/com/example/proyectouces/MenuActivity.kt
+++ app/src/main/java/com/example/proyectouces/MenuActivity.kt
@@ -1,20 +1,79 @@
 package com.example.proyectouces
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MenuActivity : AppCompatActivity() {
+    private lateinit var dbHelper: UserDBHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        dbHelper = UserDBHelper(this)
+        mostrarSocios()
+
+        val editNombre = findViewById<EditText>(R.id.editNombre)
+        val editDni = findViewById<EditText>(R.id.editDNI)
+        val btnGuardar = findViewById<Button>(R.id.btnGuardar)
+
+        val cardShowForm = findViewById<TextView>(R.id.cardShowForm)
+        val formLayout = findViewById<LinearLayout>(R.id.formLayout)
+
+        cardShowForm.setOnClickListener {
+            formLayout.visibility = View.VISIBLE
+            cardShowForm.visibility = View.GONE
+        }
+
+        btnGuardar.setOnClickListener {
+            val nombre = editNombre.text.toString().trim()
+            val dni = editDni.text.toString().trim()
+            val guardado = dbHelper.insertarSocio(nombre, dni)
+
+            if(guardado){
+                Toast.makeText(this, "Socio agregado", Toast.LENGTH_SHORT).show()
+                editNombre.text.clear()
+                editDni.text.clear()
+                mostrarSocios()
+            } else{
+                Toast.makeText(this, "Error al agregar socio", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
+    private fun mostrarSocios() {
+        val lista = dbHelper.obtenerSocios() // List<String> con nombre y DNI
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, lista)
+        val listView = findViewById<ListView>(R.id.listSocios)
+        listView.adapter = adapter
+
+        listView.setOnItemLongClickListener { _, _, position, _ ->
+            val item = lista[position]
+            val dni = item.substringAfterLast(" - ").trim()
+
+            AlertDialog.Builder(this)
+                .setTitle("Eliminar")
+                .setMessage("¿Querés eliminar a $item?")
+                .setPositiveButton("Sí") { _, _ ->
+                    dbHelper.eliminarSocioPorDni(dni)
+                    mostrarSocios() // Recarga la lista
+                    Toast.makeText(this, "Socio eliminado", Toast.LENGTH_SHORT).show()
+                }
+                .setNegativeButton("No", null)
+                .show()
+            true
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/proyectouces/UserDBHelper.kt
+++ app/src/main/java/com/example/proyectouces/UserDBHelper.kt
@@ -1,10 +1,11 @@
 package com.example.proyectouces
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteOpenHelper
 import android.database.sqlite.SQLiteDatabase
 
-class UserDBHelper(context: Context) : SQLiteOpenHelper(context, "UsuariosDB", null, 1){
+class UserDBHelper(context: Context) : SQLiteOpenHelper(context, "ClubDB", null, 2){
 
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL("""
@@ -16,10 +17,18 @@ class UserDBHelper(context: Context) : SQLiteOpenHelper(context, "UsuariosDB", n
 
         db.execSQL("INSERT INTO usuarios (nombre, contrasenia) VALUES ('admin', '1234')")
         db.execSQL("INSERT INTO usuarios (nombre, contrasenia) VALUES ('admin2', '123456')")
+
+        db.execSQL("""
+            CREATE TABLE socios (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT,
+                dni TEXT UNIQUE
+            ) """.trimIndent())
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS usuariosDB")
+        onCreate(db)
     }
 
     fun login(nombre: String, contrasenia: String) : Boolean{
@@ -30,6 +39,40 @@ class UserDBHelper(context: Context) : SQLiteOpenHelper(context, "UsuariosDB", n
         )
         val existe = cursor.count > 0
         return existe
+    }
+
+    fun insertarSocio(nombre: String, dni: String) : Boolean{
+        val db = writableDatabase
+        val valores = ContentValues().apply {
+            put("nombre", nombre)
+            put("dni", dni)
+        }
+
+        val resultado = db.insert("socios", null, valores);
+        return resultado != -1L
+    }
+
+    fun obtenerSocios(): List<String> {
+        val socios = mutableListOf<String>()
+        val db = readableDatabase
+        val cursor = db.rawQuery("SELECT nombre, dni FROM socios", null)
+
+        if (cursor.moveToFirst()) {
+            do {
+                val nombre = cursor.getString(0)
+                val dni = cursor.getString(1)
+                socios.add("$nombre - $dni")
+            } while (cursor.moveToNext())
+        }
+
+        cursor.close()
+        return socios
+    }
+
+
+    fun eliminarSocioPorDni(dni: String) {
+        val db = writableDatabase
+        db.delete("socios", "dni = ?", arrayOf(dni))
     }
 }
 
